7dd9c733b8b46286c4dbcb5149dbd11b3845d859
sonalg22 <gupta.sonal@northeastern.edu>
2023-06-18 15:54:30 -0400

    added FileHandler

--- src/main/java/cs3500/pa05/view/FileHandler.java
+++ src/main/java/cs3500/pa05/view/FileHandler.java
@@ -1,6 +1,8 @@
 package cs3500.pa05.view;
 
 import cs3500.pa05.model.Week;
+import java.io.File;
+import javafx.stage.FileChooser;
 
 /**
  * class to handle persistence
@@ -17,8 +19,34 @@ public class FileHandler {
     this.week = week;
   }
 
+  public void saveToFile() {
+    FileChooser fileChooser = new FileChooser();
+    fileChooser.setTitle("Save as .bujo File");
+    fileChooser.getExtensionFilters().add(
+        new FileChooser.ExtensionFilter(".bujo Files", "*.bujo")
+    );
+    File selectedFile = fileChooser.showSaveDialog(null);
 
+    if (selectedFile != null) {
+      // Save the Week data to the selected file
+      // Serialize the Week object and write it to the file
+      // Update the UI or show a confirmation message
+    }
+  }
 
+  public void openFile() {
+    FileChooser fileChooser = new FileChooser();
+    fileChooser.setTitle("Open .bujo File");
+    fileChooser.getExtensionFilters().add(
+        new FileChooser.ExtensionFilter(".bujo Files", "*.bujo")
+    );
+    File selectedFile = fileChooser.showOpenDialog(null);
 
+    if (selectedFile != null) {
+      // Load the Week data from the selected file
+      // Deserialize the JSON data and create a Week object
+      // Update the UI with the loaded Week data
+    }
+  }
 
 }
