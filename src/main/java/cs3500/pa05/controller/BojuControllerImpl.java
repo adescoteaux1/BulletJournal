c76072a7403317765ace36893e7b0fccc9dad4e1
adescoteaux1 <descoteaux.a@northeastern.edu>
2023-06-20 11:08:24 -0400

    handle adding a new task

--- src/main/java/cs3500/pa05/controller/BojuControllerImpl.java
+++ src/main/java/cs3500/pa05/controller/BojuControllerImpl.java
@@ -1,17 +1,33 @@
 package cs3500.pa05.controller;
 
+import static cs3500.pa05.model.DayOfWeek.FRIDAY;
+import static cs3500.pa05.model.DayOfWeek.MONDAY;
+import static cs3500.pa05.model.DayOfWeek.SATURDAY;
+import static cs3500.pa05.model.DayOfWeek.SUNDAY;
+import static cs3500.pa05.model.DayOfWeek.THURSDAY;
+import static cs3500.pa05.model.DayOfWeek.TUESDAY;
+import static cs3500.pa05.model.DayOfWeek.WEDNESDAY;
+
+import cs3500.pa05.model.Action;
+import cs3500.pa05.model.Task;
 import cs3500.pa05.model.BujoWriter;
+import cs3500.pa05.model.Day;
 import cs3500.pa05.model.FileAppendable;
 import cs3500.pa05.model.Writer;
-import cs3500.pa05.view.BojuViewImpl;
 import cs3500.pa05.view.UserInputView;
 import java.io.IOException;
 import java.nio.file.Paths;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 public class BojuControllerImpl implements BojuController {
@@ -32,12 +48,24 @@ public class BojuControllerImpl implements BojuController {
   private ToggleButton changeTheme;
   @FXML
   private Button addNote;
-  Writer writer;
+  private Writer writer;
+  private Popup popup;
+  @FXML
+  private Button createTaskButton;
+  @FXML
+  private TextField nameInput;
+  @FXML
+  private TextField descriptionInput;
+  @FXML
+  private ChoiceBox dayBox;
 
 
 
   public BojuControllerImpl(Stage stage) {
     this.stage = stage;
+    this.popup = new Popup();
+    dayBox = new ChoiceBox(FXCollections.observableArrayList(
+        MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY));
   }
 
   @Override
@@ -58,7 +86,13 @@ public class BojuControllerImpl implements BojuController {
     writer = new BujoWriter(output);
 
     //button actions
-    addTask.setOnAction(e -> addTask());
+    addTask.setOnAction(e -> {
+      try {
+        addTask();
+      } catch (IOException ex) {
+        throw new RuntimeException(ex);
+      }
+    });
     addEvent.setOnAction(e -> addEvent());
     addNote.setOnAction(e -> Note());
     changeTheme.setOnAction(e -> newTheme());
@@ -98,15 +132,30 @@ public class BojuControllerImpl implements BojuController {
     //call method to add event to bujo
   }
 
-  private void addTask() {
+  private void addTask() throws IOException {
     UserInputView uiv = new UserInputView(this);
     stage.setScene(uiv.load());
     enterTitle.setText("Enter the new Task");
 
-    enterButton.setOnAction(e -> {String newTask = enterField.getText();
-      writer.write(newTask);});
-    //call method to add task to bujo
+    FXMLLoader loader = new FXMLLoader(
+        getClass().getClassLoader().getResource("newTask.fxml"));
+    loader.setController(this);
+    Scene s = loader.load();
+    popup.getContent().add((Node)s.getRoot());
+    enterButton.setOnAction(e -> {
+      String name = nameInput.getText();
+      String desc = descriptionInput.getText();
+      Object day = dayBox.getSelectionModel().getSelectedItem();
+      Action newTask = new Task(name, desc, (Day) day, false);
+      writer.write(newTask.toString());
+      popup.hide();
+    });
 
+    popup.getContent().add(enterButton);
+  }
 
+  @FXML
+  private void makePopup() {
+    this.popup.show(this.stage);
   }
 }
