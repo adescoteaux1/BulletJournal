c7861bcd0d424e0f3aa0741a954a6df8233d71d8
sonalg22 <gupta.sonal@northeastern.edu>
2023-06-21 22:56:23 -0400

    deleted files

--- main/java/cs3500/pa05/model/json/EventJson.java
+++
@@ -1,7 +0,0 @@
-package cs3500.pa05.model.json;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import cs3500.pa05.model.Event;
-
-public record EventJson(@JsonProperty("event") Event event) {
-}
--- main/java/cs3500/pa05/model/json/TaskJson.java
+++
@@ -1,7 +0,0 @@
-package cs3500.pa05.model.json;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import cs3500.pa05.model.Task;
-
-public record TaskJson(@JsonProperty("task") Task task) {
-}
