b01139965f3fe094160269df770777bb35c1432b
adescoteaux1 <descoteaux.a@northeastern.edu>
2023-06-22 05:13:02 -0400

    Tests?

---
+++ src/test/java/cs3500/pa05/model/TestDay.java
@@ -0,0 +1,16 @@
+package cs3500.pa05.model;
+
+import cs3500.pa05.model.Event;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class TestDay {
+  @Test
+  public void testAddEvent() {
+    Event newEvent = new Event()
+    List<Event> expeted = new ArrayList<>();
+    expected.add()
+
+  }
+}
